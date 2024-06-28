383182dec12dd06073b0876d940d6f5e2d191b69
GPMila79 <lu.pshenichnikova@gmail.com>
2024-06-28 12:43:39 -0300

    Added homework_7

---
+++ homework/ludmila_pshenichnikova/homework_7/task3.py
@@ -0,0 +1,12 @@
+str1 = 'результат операции: 42'
+str2 = 'результат операции: 54'
+str3 = 'результат работы программы: 209'
+str4 = 'результат: 2'
+main_number = 10
+def final(*args):
+    number = int(args.split()[-1])
+    result = 0
+    for number in args:
+        result = number + main_number
+        return result
+    print(final(args))

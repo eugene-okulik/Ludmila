02006c15148b5558534eeb25f7157d8e77279350
GPMila79 <lu.pshenichnikova@gmail.com>
2024-06-28 12:50:30 -0300

    Fixed erros fron link

--- homework/ludmila_pshenichnikova/homework_7/task3.py
+++ homework/ludmila_pshenichnikova/homework_7/task3.py
@@ -3,6 +3,7 @@ str2 = 'результат операции: 54'
 str3 = 'результат работы программы: 209'
 str4 = 'результат: 2'
 main_number = 10
+
 def final(*args):
     number = int(args.split()[-1])
     result = 0

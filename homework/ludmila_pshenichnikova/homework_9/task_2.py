dc1aa78c8f1e074c081cc7f203fb9785f1c0ef95
GPMila79 <lu.pshenichnikova@gmail.com>
2024-07-05 07:31:40 -0300

    Added average temp

--- homework/ludmila_pshenichnikova/homework_9/task_2.py
+++ homework/ludmila_pshenichnikova/homework_9/task_2.py
@@ -2,7 +2,9 @@ temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32,
                 34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
                 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 
-new_temperatures = filter(lambda x: x > 28, temperatures)
+new_temperatures = list(filter(lambda x: x > 28, temperatures))
+average = sum(new_temperatures) / len(new_temperatures)
 print(list(new_temperatures))
 print(max(temperatures))
 print(min(temperatures))
+print(round(average, 2))

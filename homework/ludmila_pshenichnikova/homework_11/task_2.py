35684557c87d6401cdde9c0f452164ed19f7f5d1
GPMila79 <lu.pshenichnikova@gmail.com>
2024-07-08 12:00:21 -0300

    Fixed errors from link

--- homework/ludmila_pshenichnikova/homework_11/task_2.py
+++ homework/ludmila_pshenichnikova/homework_11/task_2.py
@@ -14,10 +14,10 @@ class Book:
    def __str__(self):
        return f'Название: {self.title}, Автор: {self.author}, страниц: {self.pages}'.strip()
 
+
 class School(Book):
 
-
-    def __init__ (self, title, author, pages, subject, group, task=True):
+    def __init__(self, title, author, pages, subject, group, task=True):
         super().__init__(title, author, pages)
         self.subject = subject
         self.group = group
@@ -29,11 +29,11 @@ class School(Book):
                 f'предмет: {self.subject}, класс: {self.group}{reserve_status}').strip()
 
 
-book_one = School('Aлгебра','Иванов',500,'Математика',5,True )
-book_two = School('История','Петров',200,'История',8,False)
-book_three = School('География','Сидоров',350,'География',8,False)
-book_four = School('Биология','Марков',400,'Биология',9,True)
-book_five = School('Физика','Петров',280,'Физика',10, True)
+book_one = School('Aлгебра', 'Иванов', 500, 'Математика', 5, True)
+book_two = School('История', 'Петров', 200, 'История', 8, False)
+book_three = School('География', 'Сидоров', 350, 'География', 8, False)
+book_four = School('Биология', 'Марков', 400, 'Биология', 9, True)
+book_five = School('Физика', 'Петров', 280, 'Физика', 10, True)
 
 book_four.reserve()
 
